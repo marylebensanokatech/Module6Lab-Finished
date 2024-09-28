3fa092e6ac68ead92dc43b222d8ea8c14c3a22a5
Dr. Mary Lebens <164679441+marylebensanokatech@users.noreply.github.com>
2024-09-28 16:07:51 +0000

    Initial

---
+++ Module6Lab/SuperPlayer.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Module6Lab
+{
+    internal class SuperPlayer : Player
+    {
+        // Additional properties or methods specific to SuperPlayer
+        // For example:
+        public void Fly()
+        {
+            Console.WriteLine("SuperPlayer is flying!");
+        }
+    }
+}
+            
